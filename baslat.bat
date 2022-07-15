09f1b8805bbc5e85d0299016b99106c0ed72b321
kaansdcc <101643360+kaansdcc@users.noreply.github.com>
2022-07-15 14:28:48 +0300

    aqq

---
+++ baslat.bat
@@ -0,0 +1,7 @@
+echo off
+color 0e
+title Can Bey Bot
+cls
+:a
+node index.js
+goto a
--- events/guildMemberAdd.js
+++
@@ -1,6 +0,0 @@
-const Discord = require("discord.js");
-module.exports = member => {
-
-};
-
-//Coder By EfeBey
